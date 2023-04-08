ac8d237dbea897ed11f0d2110853e4f80dd9f431
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-04-08 11:28:23 -0300

    feat: Adicionando fontes na aplicação

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import theme from "./src/theme";
 import { ThemeProvider } from "styled-components/native";
 
-import theme from "./src/theme";
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_900Black } from '@expo-google-fonts/montserrat'
+
+import { Home } from "@screens/Home";
+import { Loading } from "@components/Loading";
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_900Black });
+
   return (
     <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
 }
