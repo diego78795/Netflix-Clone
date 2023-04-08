df34af0c26d9da4e1026f108f0543357839d0fe6
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-04-08 10:43:47 -0300

    env: Criando arquivo de temas do projeto

---
+++ src/theme/index.ts
@@ -0,0 +1,27 @@
+export default {
+    colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+
+        gray_100: "#C4C4C4",
+        gray_300: "#6E6E6E",
+        gray_200: "#323232",
+        gray_400: "#121212"
+    },
+
+    font_family: {
+        montserrat_medium: "Montserrat_500Medium",
+        montserrat_semibold: "Montserrat_600SemiBold",
+        montserrat_black: "Montserrat_900Black",
+        roboto_regular: "Roboto_400Regular"
+    },
+
+    font_size: {
+        SX: 8,
+        SM: 12,
+        MD: 14,
+        LG: 16,
+        XL: 20,
+        Banner: 35
+    }
+}
