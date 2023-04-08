209e65dda0f8d65338bc658925878d0d3d26f2f1
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-04-08 12:25:33 -0300

    feat: Implementando ContainerMovies na Home com dados mockados

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,33 @@
+import { ContainerMovies } from "@components/ContainerMovies";
+
 import { Container } from "./styles";
 
 export function Home() {
+
+    const movies = [
+        {
+            id: "1",
+            uri: "https://image.tmdb.org/t/p/w300/6GE5FwsQIQKNDWkSEBVIWYx7zCK.jpg"
+        },
+        {
+            id: "2",
+            uri: "https://image.tmdb.org/t/p/w300/6GE5FwsQIQKNDWkSEBVIWYx7zCK.jpg"
+        },
+        {
+            id: "3",
+            uri: "https://image.tmdb.org/t/p/w300/6GE5FwsQIQKNDWkSEBVIWYx7zCK.jpg"
+        },
+        {
+            id: "4",
+            uri: "https://image.tmdb.org/t/p/w300/6GE5FwsQIQKNDWkSEBVIWYx7zCK.jpg"
+        }
+    ]
     return (
         <Container>
-
+            <ContainerMovies category="Originais do Netflix" movies={movies} />
+            <ContainerMovies category="Recomendados para Você" movies={movies} />
+            <ContainerMovies category="Em Alta" movies={movies} />
+            <ContainerMovies category="Romance/Drama" movies={movies} />
         </Container>
     )
 }
