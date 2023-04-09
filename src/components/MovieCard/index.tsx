166242e86946889f732e45531e29b3219ab230be
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-04-09 17:00:27 -0300

    feat: Adicionando funcionalidade de aparecer o play ao clicar no MovieCard

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,13 +1,35 @@
-import { Container, ImgMovie } from "./styles";
+import { TouchableOpacityProps } from "react-native";
 
-type Props = {
+import { ButtonPlay } from "@components/ButtonPlay";
+
+import { ButtonMenu, ButtonMenuIcon, Card, Container, ContainerActive, ImgMovie, MenuMovie } from "./styles";
+
+type Props = TouchableOpacityProps & {
     imageUri: string
+    active: boolean
 }
 
-export function MovieCard({ imageUri }: Props) {
+export function MovieCard({ imageUri, active = false, ...rest }: Props) {
     return (
         <Container>
-            <ImgMovie source={{ uri: imageUri }} />
+            <Card  {...rest}>
+                <ImgMovie source={{ uri: imageUri }} />
+            </Card>
+            {active ?
+                <ContainerActive>
+                    <ButtonPlay />
+                    <MenuMovie>
+                        <ButtonMenu>
+                            <ButtonMenuIcon name="info-outline" />
+                        </ButtonMenu>
+                        <ButtonMenu>
+                            <ButtonMenuIcon name="more-vert" />
+                        </ButtonMenu>
+                    </MenuMovie>
+                </ContainerActive>
+                :
+                <></>
+            }
         </Container>
     )
 }
--- src/components/MovieCard/styles.ts
+++ src/components/MovieCard/styles.ts
@@ -1,17 +1,51 @@
 import styled from "styled-components/native"
 
+import { MaterialIcons } from "@expo/vector-icons"
 import { TouchableOpacity, Image } from "react-native"
 
-export const Container = styled(TouchableOpacity)`
+export const Container = styled.View`
     width: 140px;
     height: 220px;
-    background-color: ${({ theme }) => theme.colors.gray_200};
     margin-right: 10px;
+`;
+
+export const Card = styled(TouchableOpacity)`
+    flex:1;
+    background-color: ${({ theme }) => theme.colors.gray_200};
 `;
 
 export const ImgMovie = styled(Image).attrs({
     resizeMode: "stretch"
 })`
+    flex:1;
+`;
+
+export const ContainerActive = styled.View`
     width: 100%;
     height: 100%;
+    position: absolute;
 `;
+
+export const MenuMovie = styled.View`
+    width: 100%;
+    height: 45px;
+    background-color: ${({ theme }) => theme.colors.gray_400};
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+export const ButtonMenu = styled(TouchableOpacity)`
+    width: 30px;
+    height: 30px;
+`;
+
+type Props = {
+    name: string;
+}
+
+export const ButtonMenuIcon = styled(MaterialIcons).attrs<Props>(({ theme, name }) => ({
+    name: name,
+    color: theme.colors.white,
+    size: 30
+}))``;
