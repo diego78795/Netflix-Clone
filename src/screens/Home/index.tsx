3ffb5771c62c293822b75be2a0cadb8f010d3de4
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-04-12 12:17:04 -0300

    feat: Implementando o FeaturedMovieBanner na tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 
 import { ContainerMovies } from "@components/ContainerMovies";
+import { FeaturedMovieBanner } from "@components/FeaturedMovieBanner";
 
 import MoviesContext from "@context/Store/MovieContext";
 
@@ -8,13 +9,16 @@ import { Container } from "./styles";
 
 export function Home() {
 
-    const { getMoviesList } = useContext(MoviesContext)
+    const { getMoviesList, getFeaturedMovie } = useContext(MoviesContext)
 
     const [moviesList, setMoviesList] = useState<Array<{ title: string, items: any }>>([])
+    const [movieFeatured, setMovieFeatured] = useState({ genres: [], backdrop_path: "", name: "" })
 
     const fetchMovies = async () => {
         let list = await getMoviesList()
+        let movie = await getFeaturedMovie()
         setMoviesList(list)
+        setMovieFeatured(movie)
     }
 
     useEffect(() => {
@@ -23,6 +27,7 @@ export function Home() {
 
     return (
         <Container>
+            <FeaturedMovieBanner movieFeatured={movieFeatured} />
             {
                 moviesList.map((movies) => {
                     return <ContainerMovies key={movies.title} category={movies.title} movies={movies.items} />
